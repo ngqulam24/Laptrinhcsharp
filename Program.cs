8991d2bf8b6f7cb8813ac47c657997bb0cfd388b
Nguyen Quang Lam <148853648+ngqulam24@users.noreply.github.com>
2024-05-30 17:36:07 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the number of integers in the array: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] array = new int[n];
+
+        Console.WriteLine("Enter the integers:");
+        for (int i = 0; i < n; i++)
+        {
+            array[i] = int.Parse(Console.ReadLine());
+        }
+
+        int maxValue = array[0];
+        for (int i = 1; i < n; i++)
+        {
+            if (array[i] > maxValue)
+            {
+                maxValue = array[i];
+            }
+        }
+
+        Console.WriteLine($"The maximum value in the array is: {maxValue}");
+    }
+}
